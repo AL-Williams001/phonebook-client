2a42a550a3b89b8d49c0a3053f7823575a2d2273
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-03-20 13:59:14 +0800

    update package.json deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/phonebook-client",
+  base: "/phonebook-client/",
   plugins: [react()],
   serever: {
     proxy: "https://phonebook-app-zpsg.onrender.com",
